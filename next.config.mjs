81f55f63b225cf50228c3ed5565660ae8853fffe
Sjd <sajjadmoeinfard1385@gmail.com>
2025-12-20 14:43:42 +0330

    Update  next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,9 @@ const nextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 60,
+    // برای لیارا - تصاویر بدون بهینه‌سازی لود می‌شوند تا مشکل حل شود
+    unoptimized: true,
+    remotePatterns: [],
   },
 
   // Security headers
